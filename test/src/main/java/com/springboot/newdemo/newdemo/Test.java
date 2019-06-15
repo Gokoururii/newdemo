ea16b17fc69969bfc1e3cdc50ed21ccdb2458add
Gokoururii <caozjaicf@gmail.com>
2019-06-15 17:35:02 +0800

    项目构建

---
+++ test/src/main/java/com/springboot/newdemo/newdemo/Test.java
@@ -0,0 +1,11 @@
+package com.springboot.newdemo.newdemo;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Service
+public class Test {
+
+    @ResponseBody
+    public void m(){}
+}
